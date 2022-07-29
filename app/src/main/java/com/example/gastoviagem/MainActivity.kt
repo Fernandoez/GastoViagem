a9c5092d77272635e223be8ec4f33abb5cd5d037
Fernando E Zimerman <fernando.ezimerman@gmail.com>
2022-07-28 22:27:47 -0300

    Configurando View Binding

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //ActivityMainBinding classe criada para ajudar mapear os elementos
+    //inflate digo para "ativar" o layout para que eu possa usar os atributos que contém nela
+    //Definimos a variavel binding como sendo global para que possa ser usada em outras funcoes
+    //que venham a aparecer e que irão interagir com nosso layout.
+    //Porém, a variável precisa ser inicializada no super.onCreate que é onde ele instancia as variáveis,
+    //faz com que ela se comporte como uma activity e torna acessível os elementyos da interface.
+    //setContentView é onde atribuímos o layot dessa activity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
